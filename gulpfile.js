dd9bc06b8894676aa1b8addcd47905a168c336f0
Bilal Quadri <bilalquadri92@gmail.com>
2014-06-18 21:27:37 -0700

    Fixup gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,49 @@
 var gulp = require('gulp')
   , bower = require('gulp-bower')
   , jshint = require('gulp-jshint')
-  , karma = require('gulp-karma')
 
 /**
  * Utitlity modules
  */
-var stylish = require('jshint-stylish')
+var karma = require('karma').server
+  , stylish = require('jshint-stylish')
+  , _ = require('lodash')
 
 /**
  * Path declarations
  */
 var paths = {
+  dependencies: [
+    'bower_components/angular/angular.js',
+    'bower_components/angular-mocks/angular-mocks.js'
+  ],
   scripts: ['src/**/*.js'],
-  tests: ['test/**/*.spec.js']
+  tests: ['test/**/*.support.js', 'test/**/*.spec.js']
+}
+paths.all = paths.dependencies.concat(paths.scripts, paths.tests)
+
+/**
+ * Configuration in common between all karma runs
+ */
+var karmaCommonConf = {
+  files: paths.all,
+  frameworks: ['jasmine'],
+  preprocessors: {
+    'src/**/*.js': ['coverage']
+  },
+  reporters: ['progress', 'coverage', 'growl'],
+  coverageReporter: {
+    reporters: [
+      { type: 'lcov', dir: 'coverage/' },
+      { type: 'text-summary' }
+    ]
+  }
 }
 
 /**
  * Task to retrieve dependencies from bower
  */
-gulp.task('bower', bower);
+gulp.task('bower', bower)
 
 /**
  * Task to lint code
@@ -35,5 +59,23 @@ gulp.task('lint', function() {
   .pipe(jshint.reporter(stylish))
 })
 
-gulp.task('test', ['bower', 'lint'], function() {
+/**
+ * Task to execute tests a single time
+ */
+gulp.task('test', ['bower', 'lint'], function(done) {
+  var testConf = _.assign({}, karmaCommonConf, {
+    browsers: ['Firefox'],
+    singleRun: true
+  })
+  karma.start(testConf, done)
+})
+
+/**
+ * Task that continuously runs tests
+ */
+gulp.task('tdd', ['bower', 'lint'], function(done) {
+  var tddConf = _.assign({}, karmaCommonConf, {
+    browsers: ['PhantomJS']
+  })
+  karma.start(tddConf, done)
 })
